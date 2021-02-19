dd7fad381bee70282c9f645cecc1fd3139c1bdcc
Ailsa Young <ailsa.young16@hotmail.co.uk>
2021-02-19 11:54:43 +0000

    some button things

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import { useState, useEffect} from "react";
 import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
+import {Link} from "react-router-dom";
 import {Container, Button} from "semantic-ui-react";
 import HeaderContainer from "./containers/HeaderContainer";
 import RecommendationsContainer from './containers/RecommendationsContainer';
@@ -10,17 +11,17 @@ function App() {
 
   return (
     <>
+    <Router>
       <Container textAlign='center'>
-        <Button href='/' floated='left' size='large' circular icon='home' />
+      <Link to="/"><Button floated='left' size='large' circular icon='home'/></Link>
         <h1>Board Game Recommendations... for YOU! </h1>
         {/* <HeaderContainer /> */}
-          <Router>
           <Switch>
             <Route exact path="/" component={RecommendationsContainer} />
             <Route path="/single-game" component={SingleGameView} />
           </Switch>
-        </Router>
       </Container>
+      </Router>
     </>
   );
 }
