428d3d6f273f5cca90df762b7ddd565773ca3190
William Anderson <willjatlas@googlemail.com>
2021-02-19 17:33:38 +0000

    init

---
+++ server/src/main/java/com/example/finalproject/server/client/Client.java
@@ -0,0 +1,12 @@
+package com.example.finalproject.server.client;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Client {
+
+    public void getTopTenGames(){
+
+    }
+
+}
---
+++ server/src/main/java/com/example/finalproject/server/secrets/Secrets.java
@@ -0,0 +1,4 @@
+package com.example.finalproject.server.secrets;
+
+public class Secrets {
+}
