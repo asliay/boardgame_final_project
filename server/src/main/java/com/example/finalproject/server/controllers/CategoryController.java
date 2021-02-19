0d3df42f219ff39eca7d70fa1cf0036a9ac69a57
Ailsa Young <ailsa.young16@hotmail.co.uk>
2021-02-19 17:12:22 +0000

    Added Category query and route for find all categories

--- server/src/main/java/com/example/finalproject/server/controllers/CategoryController.java
+++ server/src/main/java/com/example/finalproject/server/controllers/CategoryController.java
@@ -1,8 +1,14 @@
 package com.example.finalproject.server.controllers;
 
+import com.example.finalproject.server.models.Category;
 import com.example.finalproject.server.repositories.CategoryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class CategoryController {
@@ -10,5 +16,11 @@ public class CategoryController {
     @Autowired
     CategoryRepository categoryRepository;
 
+    // INDEX
 
+    @GetMapping(value="/categories")
+    public ResponseEntity<List<Category>> getAllCategories(){
+        List<Category> allCategories = categoryRepository.findAll();
+        return new ResponseEntity<>(allCategories, HttpStatus.OK);
+    }
 }
