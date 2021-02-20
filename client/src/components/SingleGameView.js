ca03d5bc890f99cb65a31638c1d71a061dc63e58
Ailsa Young <ailsa.young16@hotmail.co.uk>
2021-02-20 21:53:22 +0000

    Changed average play time to play time and added minutes on play time input

--- client/src/components/SingleGameView.js
+++ client/src/components/SingleGameView.js
@@ -22,7 +22,7 @@ const SingleGameView = (props) => {
                 <Grid.Column textAlign="left">
                         <h2> {props.location.game.name} ({props.location.game.releaseYear})</h2>
                         <p>For {props.location.game.minPlayers}-{props.location.game.maxPlayers} Players</p>
-                        <p>Average Play Time: {props.location.game.playTime} minutes</p>
+                        <p>Play Time: {props.location.game.playTime} minutes</p>
                         <p>Categories:</p><p>{gameCategories}</p>
                         <p>This is a sample description, this is a sample paragraph about a board game to see how this section looks. Board games are great, aren't they? So much fun. This is a sample description, this is a sample paragraph about a board game to see how this section looks. Board games are great, aren't they? So much fun.This is a sample description, this is a sample paragraph about a board game to see how this section looks. Board games are great, aren't they? So much fun.This is a sample description, this is a sample paragraph about a board game to see how this section looks. Board games are great, aren't they? So much fun.This is a sample description, this is a sample paragraph about a board game to see how this section looks. Board games are great, aren't they? So much fun.This is a sample description, this is a sample paragraph about a board game to see how this section looks. Board games are great, aren't they? So much fun. </p>
                 </Grid.Column>
