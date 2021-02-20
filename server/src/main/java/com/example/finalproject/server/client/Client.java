4cc169e6c4a10dbcf49124dfae51aee2a3fa5750
William Anderson <willjatlas@googlemail.com>
2021-02-20 21:09:28 +0000

    refactored client

--- server/src/main/java/com/example/finalproject/server/client/Client.java
+++ server/src/main/java/com/example/finalproject/server/client/Client.java
@@ -23,7 +23,7 @@ public class Client {
     private static ObjectMapper objectMapper = getDefaultObjectMapper();
 
     // Custom configuration for object mapper, ignores unknown properties in json.
-    // Java time module in case we want to parse times later.
+    // Java time module in case we want to parse java times later.
     private static ObjectMapper getDefaultObjectMapper(){
         ObjectMapper defaultObjectMapper = new ObjectMapper();
         defaultObjectMapper.registerModule(new JavaTimeModule());
@@ -31,25 +31,15 @@ public class Client {
         return defaultObjectMapper;
     };
 
-    // Parsing method for use on the returned json string from restTemplate.
-    public static JsonNode parse(String src) throws IOException {
+    // Parsing method for use on the returned JSON string from restTemplate.
+    private static JsonNode parse(String src) throws IOException {
          return objectMapper.readTree(src);
     }
 
-    // Method for getting top 10 games from BGA.
-    public ArrayList<BoardGame> getTopTenGames() {
+    // Generates a board games list when given a JSON string.
+    private ArrayList<BoardGame> generateBoardGames(String responseString){
 
         ArrayList<BoardGame> boardGamesOuput = new ArrayList<>();
-
-        RestTemplate restTemplate = new RestTemplate();
-
-        String responseString = restTemplate.getForObject(baseURL +
-                        "limit=10&order_by=popularity&fields=name,year_published," +
-                        "min_players,max_players,max_playtime,thumb_url,image_url" +
-                        "&client_id=" + secrets.getClient_id(),
-                        String.class);
-
-        System.out.println(responseString);
 
         try {
             JsonNode node = parse(responseString);
@@ -63,9 +53,9 @@ public class Client {
 
             for(int i = 0; i < names.size(); i++){
                 BoardGame bg = new BoardGame(names.get(i).asText(), releaseYears.get(i).asInt(),
-                                            minPlayers.get(i).asInt(), maxPlayers.get(i).asInt(),
-                                            playTimes.get(i).asInt(), thumbnailUrls.get(i).asText(),
-                                            boxImageUrls.get(i).asText());
+                               minPlayers.get(i).asInt(), maxPlayers.get(i).asInt(),
+                               playTimes.get(i).asInt(), thumbnailUrls.get(i).asText(),
+                               boxImageUrls.get(i).asText());
                 boardGamesOuput.add(bg);
             }
 
@@ -74,6 +64,20 @@ public class Client {
         }
 
         return boardGamesOuput;
+
+    }
+
+    // Gets a give number of board games from BGA by popularity.
+    public ArrayList<BoardGame> getGamesByPopularity(int numberOfGames) {
+
+        RestTemplate restTemplate = new RestTemplate();
+
+        String responseString =  restTemplate.getForObject(baseURL +
+                                 "limit="+numberOfGames+"&order_by=popularity&fields=name," +
+                                 "year_published,min_players,max_players,max_playtime," +
+                                 "thumb_url,image_url&client_id=" + secrets.getClient_id(), String.class);
+
+        return generateBoardGames(responseString);
 
     }
 
