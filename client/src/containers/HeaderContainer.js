cf842b2ed25c744c9850af44cfc41e5953c3ed24
William Anderson <willjatlas@googlemail.com>
2021-02-21 11:29:23 +0000

    centered header

--- client/src/containers/HeaderContainer.js
+++ client/src/containers/HeaderContainer.js
@@ -20,8 +20,9 @@ const HeaderContainer = () => {
             User
           </Menu.Item>
         </Link>
-          <Header as='h1' textAlign='center'>Insert Board Game Recommender App Name Here</Header>
-        
+          <div id="custom-header-contain">
+            <Header as='h1' textAlign='center'>Insert Board Game Recommender App Name Here</Header>
+          </div>
     </Menu>
   )
 }
